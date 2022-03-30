6e27f2b35ae188376538c6e4743aa8a8fa810da5
Sean Luo <shawnluo415@gmail.com>
2022-03-29 20:09:53 -0700

    done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [listing, setListing] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`/api/toons`)
+      .then((response) => {
+        console.log(response.data);
+        setListing(response.data);
+      })
+      .catch((error) => {
+        console.log("getCode error");
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Toons</h1>
+      <p>This component demonstrates fetching data from the server.</p>
+      <table width={600}>
+        <thead>
+          <tr style={{ textAlign: "left" }}>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Occupation</th>
+          </tr>
+        </thead>
+        <tbody>
+          {setListing.length &&
+            listing.map((l) => {
+              return (
+                <>
+                  <tr style={{ textAlign: "left" }}>
+                    <td>{l.id}</td>
+                    <td>
+                      {l.firstName} {l.lastName}
+                    </td>
+                    <td>{l.occupation}</td>
+                  </tr>
+                </>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 }
